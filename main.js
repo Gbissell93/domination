48a586d2cf452fee1dbd7e38c24b00d4dcc7df3a
Gbissell93 <gregorybisselledu@gmail.com>
2021-07-05 17:38:13 -0400

    first task done

---
+++ main.js
@@ -0,0 +1,7 @@
+const ul = document.querySelector('ul');
+let firstChild = ul.firstElementChild
+ function strikeThrough () {
+     firstChild.style.textDecoration = "line-through"
+ }
+
+ strikeThrough();
